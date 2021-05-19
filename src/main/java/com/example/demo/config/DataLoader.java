1c729f588e968f61535689440ee07a1cb0755d5c
mi33sa <3mi3im3@gmail/com>
2021-05-20 01:12:50 +0900

    h2でのデータベース学習を行いました。

---
+++ src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Comment;
+import com.example.demo.repository.Commentrepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+	
+	private final Commentrepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception{
+		Comment comment = new Comment();
+		comment.setContent("こんにちは");
+		repository.save(comment);
+		
+		comment = new Comment();
+		comment.setContent("テストコメント");
+		repository.save(comment);
+	}
+
+}
