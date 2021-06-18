0f362ea75e845dfd7cc5a823d4e509bd887f1983
mi33sa <3mi3im3@gmail/com>
2021-06-19 00:00:33 +0900

    データのやり取りをh2からmysqlに変更

--- java/com/example/demo/model/SiteUser.java
+++ java/com/example/demo/model/SiteUser.java
@@ -1,9 +1,11 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -16,26 +18,38 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "siteuser")
 public class SiteUser {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private long id;
 	
 	@Size(min=2,max=20)
 	@UniqueLogin
+	@Column(name = "username")
 	private String username;
 	
 	@Size(min=4,max=255)
+	@Column(name = "password")
 	private String password;
 	
 	@NotBlank
 	@Email
+	@Column(name = "email")
 	private String email;
 	
+	@Column(name = "gender")
 	private int gender;
+	
+	@Column(name = "admin")
 	private boolean admin;
+	
+	@Column(name = "role")
 	private String role;
+	
+	@Column(name = "active")
 	private boolean active = true;
 
 }
